4cabfd154e037ea87e68fef95c47f28115b4ee11
Andrew T. poe <techapoe@gmail.com>
2015-07-23 20:59:46 -0400

    HTML only pagination is fully functional

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,31 @@
 class ItemsController < ApplicationController
-  def index
-    @items = Item.all
+
+  def show
+    limit = 10
+    params[:max_pages] = current_customer.items.length / limit
+    page_number = params[:page_number].to_i
+    @items = current_customer.items.limit(limit).offset(limit * (page_number - 1))
+    respond_to do |format|
+      format.html { render template: "items/show" }
+    end
   end
+
 end
+
+
+# def index
+#   users = User.all
+#   page_number = params[:page_number].to_i
+#   limit_per_page = 20
+#   @users = User.limit(20).offset(limit_per_page  * (page_number - 1))
+# end
+#
+# def users_paginated_ajax
+#   users = User.all
+#   page_number = params[:page_number].to_i
+#   limit_per_page = 20
+#   @users = User.limit(20).offset(limit_per_page  * (page_number - 1))
+#   respond_to do |format|
+#     format.json { render json: @users }
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  get 'items', to: 'items#index'
+  get '/items/:page_number', to: 'items#show', as: 'items'
 
   devise_for :customers
 
